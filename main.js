8c5b8f134156709b1ad274f81827f6ec830526ed
fatemehfekrati <fatemefekrati@gmail.com>
2025-08-20 16:45:50 +0330

    Security_Site

--- main.js
+++ main.js
@@ -40,3 +40,18 @@ fetch('header.html')
 
 
     
+     fetch('NewProductsPic.html')
+  .then(response => {
+    if (!response.ok) throw new Error('مشکل در بارگذاری هدر');
+    return response.text();
+  })
+  .then(html => {
+
+    document.getElementById('New_Products_Pic').innerHTML= html ;
+  })
+  .catch(error => {
+    console.error('خطا:', error);
+  });
+
+
+    
