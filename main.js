064ae7bb4b29dfb85584e3f5db369f68a0199d4b
fatemehfekrati <fatemefekrati@gmail.com>
2025-09-23 16:39:46 +0330

    Del_Components

--- main.js
+++ main.js
@@ -1,57 +1,60 @@
-fetch('header.html')
-  .then(response => {
-    if (!response.ok) throw new Error('مشکل در بارگذاری هدر');
+fetch("header.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("مشکل در بارگذاری هدر");
     return response.text();
   })
-  .then(html => {
-    document.getElementById('header-container').innerHTML = html;
+  .then((html) => {
+    document.getElementById("header-container").innerHTML = html;
   })
-  .catch(error => {
-    console.error('خطا:', error);
+  .catch((error) => {
+    console.error("خطا:", error);
+  });
+
+fetch("menuHedear.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("مشکل در بارگذاری هدر");
+    return response.text();
+  })
+  .then((html) => {
+    document.getElementById("menuHedear").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("خطا:", error);
+  });
+
+fetch("Footer.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("مشکل در بارگذاری هدر");
+    return response.text();
+  })
+  .then((html) => {
+    document.getElementById("Footer_Main").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("خطا:", error);
+  });
+
+fetch("NewProductsPic.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("مشکل در بارگذاری هدر");
+    return response.text();
+  })
+  .then((html) => {
+    document.getElementById("New_Products_Pic").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("خطا:", error);
   });
 
 
-
-  fetch('menuHedear.html')
-  .then(response => {
-    if (!response.ok) throw new Error('مشکل در بارگذاری هدر');
+  fetch("LeaveMessage.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("مشکل در بارگذاری هدر");
     return response.text();
   })
-  .then(html => {
-    document.getElementById('menuHedear').innerHTML = html;
+  .then((html) => {
+    document.getElementById("Leave_Message").innerHTML = html;
   })
-  .catch(error => {
-    console.error('خطا:', error);
+  .catch((error) => {
+    console.error("خطا:", error);
   });
-
-
-    fetch('Footer.html')
-  .then(response => {
-    if (!response.ok) throw new Error('مشکل در بارگذاری هدر');
-    return response.text();
-  })
-  .then(html => {
-
-    document.getElementById('Footer_Main').innerHTML= html ;
-  })
-  .catch(error => {
-    console.error('خطا:', error);
-  });
-
-
-    
-     fetch('NewProductsPic.html')
-  .then(response => {
-    if (!response.ok) throw new Error('مشکل در بارگذاری هدر');
-    return response.text();
-  })
-  .then(html => {
-
-    document.getElementById('New_Products_Pic').innerHTML= html ;
-  })
-  .catch(error => {
-    console.error('خطا:', error);
-  });
-
-
-    
