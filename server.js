7375d655a131a17fdb17c9419f1298bf5ebf3dd2
fatemehfekrati <fatemefekrati@gmail.com>
2025-12-09 17:08:54 +0330

    Contact Page

--- server.js
+++ server.js
@@ -4,41 +4,59 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public")); // فایل‌های HTML/CSS/JS از این پوشه لود می‌شن
+app.get("/contact-us_d2", (req, res) => {
+  res.sendFile(__dirname + "/public/contact-us_d2.html");
+});
+app.get("/Quanta_Libs", (req, res) => {
+  res.sendFile(__dirname + "/public/Quanta_Libs.html");
+});
 
 
-// اینجا اطلاعات SMTP خودت رو بگذار (بعد از اینکه رمز رو عوض کردی!)
+// ============= SMTP تنظیمات ===============
+// فقط این دو مقدار رو تغییر بده: user + pass
+
 const transporter = nodemailer.createTransport({
-  host: "mail.spectoshimatsu.com",
+  host: "server146c.irwebspace.com",   // هاست واقعی SMTP شما
   port: 587,
   secure: false,
   auth: {
-    user: "moro@spectoshimatsu.com",
-    pass: "PASSWORD"
+    user: "moro@spectoshimatsu.com",   // ایمیلی که ازش ارسال می‌کنی
+    pass: "z9pKifTW725SI0ov"              // پسورد جدیدی که در DirectAdmin ست کردی
   },
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false          // چون SSL هاست اشتراکیه و دامنه فرق داره
   }
 });
 
+// ============= ارسال فرم ===============
 app.post("/send", async (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, phone ,industry ,message } = req.body;
 
   try {
+    console.log("sending to", "moro@spectoshimatsu.com");
     await transporter.sendMail({
       from: `"فرم سایت" <moro@spectoshimatsu.com>`,
-      to: "fatemefekrati@gmail.com",  // ایمیل کارفرما
+      to: "moro@spectoshimatsu.com",         // گیرنده (کارفرما)
       subject: "اطلاعات جدید فرم",
       html: `
-        <p><b>نام:</b> ${name}</p>
-        <p><b>ایمیل:</b> ${email}</p>
+        <p><b>name:</b> ${name}</p>
+        <p><b>email:</b> ${email}</p>
+        <p><b>phone:</b> ${phone}</p>
+       <p><b>industry:</b> ${industry}</p>
+        <p><b>Message:</b> ${message}</p>
       `
     });
 
-    res.send("ارسال شد، برو حالشو ببر.");
+    res.send("✔️ ");
   } catch (err) {
-    console.error(err);
-    res.status(500).send("خراب شد، ولی دنیا که تموم نشد.");
+    console.error("SMTP ERROR:", err);
+    res.status(500).send("❌");
   }
 });
-app.use(express.static("public"));
-app.listen(3000, () => console.log("Server running on port 3000"));
+
+// ============= اجرای سرور ===============
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
